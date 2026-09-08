810401b627dcfcf0a5a7ec61f1fb06211620c251
zeekoorg <zeekoalmekhlafe2@gmail.com>
2026-09-08 15:28:38 +0300

    Update SplashActivity.java

--- app/src/main/java/zeeko/org/royal2026/SplashActivity.java
+++ app/src/main/java/zeeko/org/royal2026/SplashActivity.java
@@ -2,39 +2,50 @@ package zeeko.org.royal2026;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SplashActivity extends AppCompatActivity {
+import eightbitlab.com.blurview.BlurView;
+import eightbitlab.com.blurview.RenderScriptBlur;
 
-    private static final int SPLASH_DELAY = 3000; // 3 ثواني
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        // استخدام Handler للانتظار 3 ثواني قبل الانتقال
+        // تهيئة الزجاج
+        View decorView = getWindow().getDecorView();
+        ViewGroup rootView = (ViewGroup) decorView.findViewById(android.R.id.content);
+        Drawable windowBackground = decorView.getBackground();
+        setupBlurView(findViewById(R.id.blurSplash), rootView, windowBackground);
+
+        // الانتظار 3 ثواني
         new Handler(Looper.getMainLooper()).postDelayed(() -> {
-            
-            // فتح ملف الإعدادات المحفوظة للتحقق
             SharedPreferences prefs = getSharedPreferences("RoyalAppPrefs", MODE_PRIVATE);
             boolean isFirstTime = prefs.getBoolean("isFirstTime", true);
 
             if (isFirstTime) {
-                // إذا كانت أول مرة -> شاشة اللغات
                 startActivity(new Intent(SplashActivity.this, LanguageActivity.class));
             } else {
-                // إذا لم تكن أول مرة -> شاشة التحقق من الاتصال
                 startActivity(new Intent(SplashActivity.this, NetworkCheckActivity.class));
             }
-            
-            // إغلاق شاشة البداية حتى لا يعود إليها المستخدم عند الضغط على زر الرجوع
             finish();
+        }, 3000);
+    }
 
-        }, SPLASH_DELAY);
+    private void setupBlurView(BlurView blurView, ViewGroup rootView, Drawable windowBackground) {
+        if (blurView != null) {
+            blurView.setupWith(rootView, new RenderScriptBlur(this))
+                    .setFrameClearDrawable(windowBackground)
+                    .setBlurRadius(15f);
+        }
     }
 }
